235ccd77fc96532bbac36810b586833050d6ce58
Yuki <gigibengal14@gmail.com>
2025-09-12 15:55:49 +0700

    update fix 404

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig} */
+// next.config.js (atau next.config.mjs)
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: {
+    // Pastikan path glob mengarah ke folder konten kamu
+    outputFileTracingIncludes: {
+      // berlaku untuk semua route
+      '*': ['./content/articles/**'], 
+      // kalau kontenmu beda, ganti pathnya
+      // '*': ['./data/posts/**'],
+    },
   },
 };
-
 module.exports = nextConfig;
